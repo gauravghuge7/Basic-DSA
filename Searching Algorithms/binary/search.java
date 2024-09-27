4f4bdab5322fb3f0e54d2414bf7ba3a88f564067
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-27 15:52:05 +0530

    only pushing the code

--- Searching Algorithms/binary/search.java
+++ Searching Algorithms/binary/search.java
@@ -3,18 +3,20 @@ for(int i = 0; i < n; i++) {
    int end = n - 1;
 
    while(start <= end) {
-       int mid = (start + end) / 2;
 
-       if(numbers[i] + numbers[mid] == target) {
-           ans[0] = i + 1;  // Convert to 1-based index
-           ans[1] = mid + 1; // Convert to 1-based index
-           return ans;
-       } 
-       else if(numbers[i] + numbers[mid] < target) {
-           start = mid + 1; // Move the start pointer to the right
-       } 
-       else {
-           end = mid - 1; // Move the end pointer to the left
-       }
+    
+    int mid = start + (start + end) / 2;
+
+    if(numbers[i] + numbers[mid] == target) {
+        ans[0] = i + 1;  // Convert to 1-based index
+        ans[1] = mid + 1; // Convert to 1-based index
+        return ans;
+    } 
+    else if(numbers[i] + numbers[mid] < target) {
+        start = mid + 1; // Move the start pointer to the right
+    } 
+    else {
+        end = mid - 1; // Move the end pointer to the left
+    }
    }
 }
